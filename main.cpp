ecd1409e1f0850ac14063f9e373b062cdbe2e035
Pierce <piercerhamstine@gmail.com>
2022-02-09 23:11:48 -0600

    Problem make/cpp automated creation

--- main.cpp
+++ main.cpp
@@ -2,15 +2,38 @@
 #include <fstream>
 #include <iostream>
 
+std::string StrToLower(std::string str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char  c){return std::tolower(c);});
+
+    return str;
+}
+
 int main()
 {
     std::string dirName;
-    std::string path;
 
     std::cout << "Enter Problem Name: ";
     std::cin >> dirName;
 
-    path = "./"+dirName;
-    
-    std::filesystem::create_directory(path);
+    std::filesystem::create_directory("./"+dirName);
+
+    std::string path = "./"+dirName+"/";
+    std::fstream problemFile;
+    std::fstream makeFile;
+
+    std::cout << path+StrToLower(dirName)+".cpp";
+
+    problemFile.open(path+StrToLower(dirName)+".cpp", std::ios_base::out | std::ios_base::trunc);
+    problemFile.close();
+
+    makeFile.open(path+"MakeFile", std::ios_base::out | std::ios_base::trunc);
+    if(makeFile.is_open())
+    {
+        makeFile << "CXXFLAGS=-std=c++17\n";
+        makeFile << "\n" << StrToLower(dirName) << ":\n";
+        makeFile << "\tg++ $(CXXFLAGS) -o $@ $@.cpp";
+        makeFile.close();
+    }
+    else{makeFile.close();}
 };
