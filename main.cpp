1efecd0f8b27928dee80f59053c0052820bfebae
Pierce <piercerhamstine@gmail.com>
2022-02-15 10:05:25 -0600

    main refactor

--- main.cpp
+++ main.cpp
@@ -11,12 +11,42 @@ std::string StrToLower(std::string str)
 
 const std::string defaultCPP = "#include <iostream>\n\nint main()\n{\n\n}";
 
+void CreateCPP(std::fstream& problemFile, const std::string& path, const std::string& dirName)
+{
+    problemFile.open(path+StrToLower(dirName)+".cpp", std::ios_base::out | std::ios_base::trunc);
+    if(problemFile.is_open())
+    {
+        problemFile << defaultCPP;
+    }
+    problemFile.close();
+}
+
+void CreateMake(std::fstream& makeFile, const std::string& path, const std::string& dirName)
+{
+    makeFile.open(path+"MakeFile", std::ios_base::out | std::ios_base::trunc);
+    if(makeFile.is_open())
+    {
+        makeFile << "CXXFLAGS=-std=c++17\n";
+        makeFile << "\n" << StrToLower(dirName) << ":\n";
+        makeFile << "\tg++ $(CXXFLAGS) -o $@ $@.cpp";
+        makeFile.close();
+    }
+    makeFile.close();
+}
+
+void CreateReadME()
+{
+
+}
+
 int main()
 {
     // Folder name
     std::string dirName;
     // Folder Path
     std::string path;
+    // Descrition of problem;
+    std::string probDesc;
     // CPP File
     std::fstream problemFile;
     // MakeFile
@@ -25,7 +55,7 @@ int main()
     std::cout << "Enter Problem Name: ";
     std::cin >> dirName;
 
-    // Check
+    // Check if directory exists
     if(std::filesystem::is_directory("./"+dirName))
     {
         std::cout << "Directory already exists.";
@@ -38,27 +68,8 @@ int main()
 
         std::cout << path+StrToLower(dirName)+".cpp";
 
-        problemFile.open(path+StrToLower(dirName)+".cpp", std::ios_base::out | std::ios_base::trunc);
-        if(problemFile.is_open())
-        {
-            problemFile << defaultCPP;
-        }
-        problemFile.close();
 
-        makeFile.open(path+"MakeFile", std::ios_base::out | std::ios_base::trunc);
-        if(makeFile.is_open())
-        {
-            makeFile << "CXXFLAGS=-std=c++17\n";
-            makeFile << "\n" << StrToLower(dirName) << ":\n";
-            makeFile << "\tg++ $(CXXFLAGS) -o $@ $@.cpp";
 
-            if(probDesc)
-            {
 
-            }
-
-            makeFile.close();
-        }
-        makeFile.close();
     }
 };
