98659ad0c0809a78120f0202892e461b6554fb36
Pierce <piercerhamstine@gmail.com>
2022-02-18 16:12:16 -0600

    AddTwoNums

--- AddTwoNums/addtwonums.cpp
+++ AddTwoNums/addtwonums.cpp
@@ -11,30 +11,66 @@
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
 {
-    // While either list has nodes, we continue looping.
-    while(l1->next || l2->next)
-    {
-        int addRes = 0;
+    ListNode* res = new ListNode(0);
+    ListNode* temp = res;
+    int carryOver = 0;
 
-        if(l1)
+    // While either list has nodes or we have a carry, we continue looping.
+    while(l1 != nullptr || l2 != nullptr || carryOver > 0)
+    {
+        ListNode* next = new ListNode(0);
+        int addRes = carryOver;
+        carryOver = 0;
+
+        if(l1 != nullptr)
         {
             addRes += l1->val;
+            l1 = l1->next;
         }
-
-        if(l2)
+        if(l2 != nullptr)
         {
             addRes += l2->val;
+            l2 = l2->next;
         }
 
         // Check if we need to handle a carry-over.
         if(addRes >= 10)
         {
-            // Handle carry over.
+            temp->val = (addRes % 10);
+
+            while(addRes >= 10)
+            {
+                addRes = addRes-10;
+                carryOver += 1;
+            }
         }
+        else
+        {
+            temp->val = addRes;
+        }
+
+        temp->next = next;
+        temp = temp->next;
     }
+
+    return res;
 };
 
 int main()
 {
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
 
+    ListNode* l3 = addTwoNumbers(l1, l2);
+
+    while(l3 != nullptr)
+    {
+        std::cout << l3->val;
+
+        l3 = l3->next;
+    }
+
+    return 0;
 };
