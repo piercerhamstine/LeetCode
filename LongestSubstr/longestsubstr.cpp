b1c55cdb1b337746b222f032eb208de8688b496f
Pierce <piercerhamstine@gmail.com>
2022-02-21 23:15:44 -0600

    LongestSubStr

--- LongestSubstr/longestsubstr.cpp
+++ LongestSubstr/longestsubstr.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
+
+int lengthOfLongestSubstring(std::string s)
+{
+    int longestsslength = 0;
+
+    const char* charArr = s.c_str();
+
+    for(int i = 0; i < s.length(); ++i)
+    {
+    };
+
+    return longestsslength;
+}
 
 int main()
 {
+    lengthOfLongestSubstring("Fart");
 }
