c48fadec64f2972d23593f3b6ecaaec2701abdcb
Pierce <piercerhamstine@gmail.com>
2022-02-23 19:57:39 -0600

    LongestSubStr

--- LongestSubstr/longestsubstr.cpp
+++ LongestSubstr/longestsubstr.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+bool isUnique(char c, std::vector<char>& chars)
+{
+    for(int i = 0; i < chars.size(); ++i)
+    {
+        if(chars[i] == c)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int lengthOfLongestSubstring(std::string s)
 {
@@ -7,8 +21,12 @@ int lengthOfLongestSubstring(std::string s)
 
     const char* charArr = s.c_str();
 
+    std::vector<std::string> subStrings;
+    std::vector<char> uniqueChar;
+
     for(int i = 0; i < s.length(); ++i)
     {
+
     };
 
     return longestsslength;
