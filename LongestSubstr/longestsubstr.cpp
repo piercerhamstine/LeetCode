4dfd4b44d93817ac2f4a02fb6ed84dd9bf0ea680
Pierce <piercerhamstine@gmail.com>
2022-05-20 16:20:06 -0500

    LongestSubStr finished

--- LongestSubstr/longestsubstr.cpp
+++ LongestSubstr/longestsubstr.cpp
@@ -2,12 +2,70 @@
 #include <string>
 #include <vector>
 
+bool isUnique(char c, std::vector<char>& chars)
+{
+    for(int i = 0; i < chars.size(); ++i)
+    {
+        if(chars[i] == c)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int LengthLongestSubstring(std::string input)
 {
+        if(input.length() <= 0)
+        {
+            return 0;
+        };
+        
+        int longestsslength = 0;
 
+        const char* charArr = input.c_str();
+
+        std::vector<std::string> subStrings;
+        std::vector<char> uniqueChar;
+
+        for(std::size_t i = 0; i < input.length(); ++i)
+        {
+            for(std::size_t j = i; j < input.length(); ++j)
+            {
+
+                if(isUnique(charArr[j], uniqueChar))
+                {
+                    uniqueChar.push_back(charArr[j]);
+                }
+                else
+                {
+                   break;
+                };
+
+                std::cout << charArr[j];
+            }
+            subStrings.push_back(std::string(uniqueChar.begin(), uniqueChar.end()));
+            uniqueChar.clear();
+
+            std::cout << std::endl;
+        };
+
+        longestsslength = subStrings[0].length();
+        for(std::size_t i = 0; i < subStrings.size(); ++i)
+        {
+            if(subStrings[i].length() > longestsslength)
+            {
+                longestsslength = subStrings[i].length();
+            }
+
+            //std::cout << subStrings[i] << std::endl;
+        };
+
+        return longestsslength;
 };
 
 int main()
 {
-    std::cout << LengthOfLongestSubstring("b") << std::endl;
+    std::cout << LengthLongestSubstring("") << std::endl;
 };
